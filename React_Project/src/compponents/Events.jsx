a10378251ed50a28b0fc2b9122e46465a35a65cc
onkarkarande77 <karandeonkar05@gmail.com>
2026-07-12 21:59:00 +0530

    Update Events.jsx

--- React_Project/src/compponents/Events.jsx
+++ React_Project/src/compponents/Events.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Events = () => {
     const showAlert=()=>{
         alert("This my new brand new alert")
-      }
+    }
   return (<>
       <h1>Events</h1>
       <button onClick={showAlert}>click</button>
